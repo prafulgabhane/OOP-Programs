f6ef6ee9858994a8a4412c872c01b7c1d947849f
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-07 09:35:26 +0530

    Add files via upload

--- InventoryDetails.cs
+++ InventoryDetails.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 namespace Object_Oriented_Programs
 {
-	public class InventoryDetails
-	{
-		public InventoryDetails()
-		{
-		}
-	}
-}
+    public class InventoryDetails
+    {
+        public class InventoryDetails
+        {
+            public List<TypesOfStock> typesOfStock;
+        }
+        public class TypesOfStock
+        {
+            public string name;
+            public int NoOfshare;
+            public int price;
+        }
 
+    }
+}
