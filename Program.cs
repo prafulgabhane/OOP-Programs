004d1fdf8da1f10794644b10638922bfb2b10d3e
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-07 09:15:50 +0530

    Added file to remote

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Object_Oriented_Programs;
+
+namespace InventoryManagementDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Object Oriented Program");
+
+            string path = "Macintosh HD/Users/prafulgabhane/Projects/Object Oriented Programs/Object Oriented Programs/Inventory.json";
+            ReadData obj = new ReadData();
+            var Info = obj.Read(path);
+
+            //Storing the multiple values of Array in List.
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Rice is:");
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < Info.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfRice[i].name);
+                Console.WriteLine(Info.typesOfRice[i].weight);
+                Console.WriteLine(Info.typesOfRice[i].price);
+
+                int val = Info.typesOfRice[i].weight * Info.typesOfRice[i].price;
+                Console.WriteLine("The price for " + Info.typesOfRice[i].weight + " kg is " + val);
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Pulses is:");
+            Console.WriteLine("--------------------");
+
+
+            for (int i = 0; i < Info.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfPulses[i].name);
+                Console.WriteLine(Info.typesOfPulses[i].weight);
+                Console.WriteLine(Info.typesOfPulses[i].price);
+
+                int val = Info.typesOfPulses[i].weight * Info.typesOfPulses[i].price;
+                Console.WriteLine("The price for " + Info.typesOfPulses[i].weight + " kg is " + val);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Wheat is:");
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < Info.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfWheat[i].name);
+                Console.WriteLine(Info.typesOfWheat[i].weight);
+                Console.WriteLine(Info.typesOfWheat[i].price);
+
+                int val = Info.typesOfWheat[i].weight * Info.typesOfWheat[i].price;
+                Console.WriteLine("The price for " + Info.typesOfWheat[i].weight + " kg is " + val);
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
